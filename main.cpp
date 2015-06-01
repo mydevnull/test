69b44da99f9112f96133fb14ef875bc8b07f3d03
mydevnull <inequity@openaliasbox.org>
2015-06-01 05:42:57 +0200

    formatting fixes

--- main.cpp
+++ main.cpp
@@ -4,23 +4,21 @@ class Birdie
 {
  
 public:
-    
+
     Birdie()
     {
         std::cout << "entered constructor" << std::endl;
     }
-            
+
     ~Birdie()
     {
         std::cout << "entered destructor" << std::endl;
     }
 };
 
-int main ( int argc, const char * argv[] )
+int main( int argc, const char * argv[] )
 {
-    
     Birdie b;
     
-    std::cout << "HOOT HOOT";
-
+    std::cout << "HOOT HOOT" << std::endl;
 }
